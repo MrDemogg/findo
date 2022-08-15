5b95b0c7ac630c1317090d0751fc6dcfa38abeb4
demog <dg08gg@mail.ru>
2022-08-15 12:31:40 +0300

    Заготовка 36 блоков и СиэСэС (хых) для них

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.css';
+import { Component } from 'react'
 
-
+class App extends Component {
+  state = {
+    field: [{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false},{class: 'closedBlock', keepO: false}]
+  }
+  render() {
+    return (
+      <div className="field">
+      </div>
+    )
+  }
+}
 
 export default App;
